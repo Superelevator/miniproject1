c742f4ee45faa98b265f23acbd7d22bf18cac894
Jonathan Yang <jontwister99@gmail.com>
2026-09-22 09:06:51 -0400

    scrabble scoring functions

--- miniproject.py
+++ miniproject.py
@@ -1,1 +1,40 @@
 # hi
+
+def score_word(word):
+    letterPoints = {
+    "a":  1,
+    "b":  3,
+    "c":  3,
+    "d":  2,
+    "e":  1,
+    "f":  4,
+    "g":  2,
+    "h":  4,
+    "i":  1,
+    "j":  8,
+    "k":  5,
+    "l":  1,
+    "m":  3,
+    "n":  1,
+    "o":  1,
+    "p":  3,
+    "q": 10,
+    "r":  1,
+    "s":  1,
+    "t":  1,
+    "u":  1,
+    "v":  4,
+    "w":  4,
+    "x":  8,
+    "y":  4,
+    "z": 10,
+}
+
+    word_total = 0
+    for letter in word:
+        word_total += letterPoints[letter]
+
+    print(word_total)
+    
+
+score_word("salutations")
